de138aba69708e6050b56136bd3e4ca3d4f8ceba
OmAditya <omaditya567@gmail.com>
2026-03-26 11:54:10 +0530

    Implement UC7: Refactor greeting to use String.join

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -3,19 +3,8 @@ public class HelloApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            // Start building the greeting string
-            String greeting = "Hello, ";
-
-            // Append each name with a trailing delimiter
-            for (String name : args) {
-                greeting += name + ", ";
-            }
-
-            // Remove the final trailing ", " (which is 2 characters long)
-            greeting = greeting.substring(0, greeting.length() - 2);
-
-            // Add the final exclamation mark and print
-            System.out.println(greeting + "!");
+            String namesList = String.join(", ", args);
+            System.out.println("Hello, " + namesList + "!");
         }
     }
 }
